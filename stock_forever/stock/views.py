1a4f6f0f72fc76a70f795d7df20691d5cdb29701
manuel <manuelgrillo73@gmail.com>
2022-02-17 19:25:40 -0300

    antes del merge

--- stock_forever/stock/views.py
+++ stock_forever/stock/views.py
@@ -1,3 +1,4 @@
+from unicodedata import category
 from urllib import response
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
@@ -16,15 +17,27 @@ def new(request):
     return render(request, "stock/new.html",{})
 
 def add(request):
-    product_list = Product.objects.all()
-    added="Product Added Succesfully"
-    #opcion = request.GET["category"]
-    price = request.POST["price"]
-    opcion = request.POST["category"]
-    return render(request, "stock/add.html",{
-        'product_list' : product_list,
-        'added': "added succesfully",
-        "opcion":opcion,
-        "price": price
+    #product_list = Product.objects.all()    
+    product = Product()
+    product.name = request.POST["name"]
+    product.category = request.POST["category"]
+    product.material = request.POST["material"]
+    product.in_stock = request.POST["in_stock"]
+    product.price = request.POST["price"]
+    if request.POST["sell_price"]:
+        product.sell_price = request.POST["sell_price"]
+    else: 
+        product.sell_price = 0
+    product.sug_price = float(product.price) * 2
+    product.save()
 
+
+    return render(request, "stock/add.html",{
+        'name' : product.name,
+        'category': product.category,
+        'material': product.material,
+        'in_stock' : product.in_stock,
+        'price' : product.price,
+        'sell_price' : product.sell_price,
+        'sug_price' : product.sug_price
     })
