3a934258412553968a01d9195f85e70056752a76
manuel <manuelgrillo73@gmail.com>
2022-03-03 21:52:15 -0300

    se empezo a agregar funcionalidades a la app sells, aun falta progreso

--- stock_forever/sells/urls.py
+++ stock_forever/sells/urls.py
@@ -9,14 +9,15 @@ urlpatterns = [
     #ex: /sells/
     path("",  views.index, name= "index"),
     #ex: /sells/5/update_saved/
-    path("<int:sell_id>/update/saved", views.save_update, name= "save_update"),
-    #ex: /sells/5/delete/   falta actualizar
-    path("update-delete/",  views.update_or_delete, name= "update_or_delete"),
-    #ex: /sells/new     falta actualizar
-    path("new/", views.new, name="new"),
-    #ex /sells/add
-    path("new/added/",views.add, name= "add"),
-    #ex /sells/5/product_deleted
-    path("<int:sell_id>/product-deleted", views.confirm_detele, name="confirm_delete")
+    # path("<int:sell_id>/update/saved", views.save_update, name= "save_update"),
+    # #ex: /sells/5/delete/   falta actualizar
+    # path("update-delete/",  views.update_or_delete, name= "update_or_delete"),
+    # #ex: /sells/new     falta actualizar
+    path("new/1", views.new_1, name="new_1"),
+    path("new/2", views.new_2, name="new_2"),
+    # #ex /sells/add
+    # path("new/added/",views.add, name= "add"),
+    # #ex /sells/5/product_deleted
+    # path("<int:sell_id>/product-deleted", views.confirm_detele, name="confirm_delete")
     
 ]
--- stock_forever/sells/views.py
+++ stock_forever/sells/views.py
@@ -1,4 +1,7 @@
 from cgitb import reset
+from http import client
+from itertools import product
+import re
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
@@ -6,69 +9,99 @@ from django.urls import reverse
 
 
 from .models import Sell
+from stock.models import Product
+from clients.models import Client
+
 
 
 def index(request):
     sells_list = Sell.objects.all()
+    
     return render(request, "sells/index.html",{
         'sells_list' : sells_list
     })
 
-def new(request):
-    return render(request, "sells/new.html",{})
+def new_1(request):
+    clients_list = Client.objects.all()
+    products_list = Product.objects.all()
+     
+    return render(request, "sells/new_1.html",{
+        'clients_list' : clients_list
+    })
+
+def new_2(request):
+    clients_list = Client.objects.all()
+    products_list = Product.objects.all()
+    if request.POST["client"]:
+        client_name = request.POST["client"]
+    else: 
+        client_name = ""
+    quantity = request.POST["quantity"]
+    quantity_list = range(int(quantity))
+    return render(request, "sells/new_2.html",{
+        'clients_list' : clients_list,
+        'client_name' : client_name,
+        'product_list' : products_list,
+        'quantity_list' : quantity_list
+        
+    })
 
 def add(request): 
-    sell = Sell()
-    sell.name = request.POST["name"]
-    sell.address = request.POST["address"]
-    if request.POST["tlf"]:
-        sell.tlf = request.POST["tlf"]
-    if request.POST["debt"]:
-        sell.debt = request.POST["debt"]
-    sell.save()
+    pass
+    # sell = Sell()
+    # sell.name = request.POST["name"]
+    # sell.address = request.POST["address"]
+    # if request.POST["tlf"]:
+    #     sell.tlf = request.POST["tlf"]
+    # if request.POST["debt"]:
+    #     sell.debt = request.POST["debt"]
+    # sell.save()
 
-    return render(request, "sells/add.html",{
-        'sell':sell
-    })
+    # return render(request, "sells/add.html",{
+    #     'sell':sell
+    # })
     
 
 def update_or_delete(request):
-    sells_list = Sell.objects.all()
-    try:
-        sell = get_object_or_404(Sell, pk=request.POST["choice"])
-    except (KeyError, Sell.DoesNotExist):
-                return render(request, "sells/index.html", {
-                    'sells_list':sells_list,
-                    "error_message": "No elegiste una venta"
-                })
-    else:
+    pass
+    # sells_list = Sell.objects.all()
+    # try:
+    #     sell = get_object_or_404(Sell, pk=request.POST["choice"])
+    # except (KeyError, Sell.DoesNotExist):
+    #             return render(request, "sells/index.html", {
+    #                 'sells_list':sells_list,
+    #                 "error_message": "No elegiste una venta"
+    #             })
+    # else:
 
-        if request.POST["action"] == "Editar":
-            return render(request, "sells/update.html",{
-                'sell' : sell
-            })
-        elif request.POST["action"] == "Eliminar":
-            return render(request, "sells/delete.html",{
-                'sell' : sell
-            })
+    #     if request.POST["action"] == "Editar":
+    #         return render(request, "sells/update.html",{
+    #             'sell' : sell
+    #         })
+    #     elif request.POST["action"] == "Eliminar":
+    #         return render(request, "sells/delete.html",{
+    #             'sell' : sell
+    #         })
         
 def save_update(request, sell_id):
-    sell = get_object_or_404(Sell, pk=sell_id)
-    sell.name = request.POST["name"]
-    sell.address = request.POST["address"]
-    if request.POST["tlf"]:
-        sell.tlf = request.POST["tlf"]
-    if request.POST["debt"]:
-        sell.debt = request.POST["debt"]
-    sell.save()
-    return render(request, "sells/update_saved.html",{
-        'sell' : sell
-    })
+    pass
+    # sell = get_object_or_404(Sell, pk=sell_id)
+    # sell.name = request.POST["name"]
+    # sell.address = request.POST["address"]
+    # if request.POST["tlf"]:
+    #     sell.tlf = request.POST["tlf"]
+    # if request.POST["debt"]:
+    #     sell.debt = request.POST["debt"]
+    # sell.save()
+    # return render(request, "sells/update_saved.html",{
+    #     'sell' : sell
+    # })
 
 def confirm_detele(request, sell_id):
-    if request.POST["action"] == "Eliminar":
-        sell = get_object_or_404(Sell, pk=sell_id)
-        sell.delete()
-        return render(request, "sells/client_deleted.html",{})
-    else:
-        return HttpResponseRedirect(reverse("sells:index"))
+    pass
+    # if request.POST["action"] == "Eliminar":
+    #     sell = get_object_or_404(Sell, pk=sell_id)
+    #     sell.delete()
+    #     return render(request, "sells/client_deleted.html",{})
+    # else:
+    #     return HttpResponseRedirect(reverse("sells:index"))
